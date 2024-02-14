99bc9e6ce4991662aadfad7be3e1549dbf40325b
Oswald Graae <78157610+Selfsson-Dev@users.noreply.github.com>
2024-02-14 16:18:17 +0100

    Networking Cam Player working (Lan)

--- Assets/Scripts/Networking/Server Auth Movement/ServerPlayerMovement.cs
+++ Assets/Scripts/Networking/Server Auth Movement/ServerPlayerMovement.cs
@@ -1,17 +1,41 @@
-using StarterAssets;
 using Unity.Netcode;
 using UnityEngine;
-
+using Cinemachine;
+using Unity.Mathematics;
 public class ServerPlayerMovement : NetworkBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private Transform playerTransform;
-
+    [SerializeField] private CinemachineVirtualCamera cv;
+    [SerializeField] private AudioListener listener;
+    [SerializeField] private float rotationSpeed = 0.1f;
+    [SerializeField] private float accumulatedRotation;
     public CharacterController cc;
+
+    private MyPlayerInput playerInput;
+
+    private void Start()
+    {
+        playerInput = new();
+        playerInput.Enable();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if(IsOwner)
+        {
+            listener.enabled = true;
+            cv.Priority = 1;
+        }
+        else
+        {
+            cv.Priority = 0;
+        }
+    }
 
     private void Update()
     {
-        var moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        var moveInput = playerInput.Player.Movement.ReadValue<Vector2>();
 
         if(IsServer && IsLocalPlayer)
         {
@@ -21,6 +45,19 @@ public class ServerPlayerMovement : NetworkBehaviour
         {
             MoveServerRpc(moveInput);
         }
+
+        if(playerInput.Player.RightClick.ReadValue<float>() > 0)
+        {
+            var mouseDelta = playerInput.Player.Look.ReadValue<Vector2>();
+            if(IsServer && IsLocalPlayer)
+            {
+                LookAround(mouseDelta);
+            }
+            else if(IsClient && IsLocalPlayer)
+            {
+                LookAroundServerRpc(mouseDelta);
+            }
+        }
     }
 
     private void Move(Vector2 inputVector)
@@ -29,9 +66,24 @@ public class ServerPlayerMovement : NetworkBehaviour
         cc.Move(calcMove * moveSpeed * Time.deltaTime);
     }
 
-    [ServerRpc]
-    private void MoveServerRpc(Vector3 inputVector)
+    private void LookAround(Vector2 _lookVector)
     {
-        Move(inputVector);
+        float rotationAmount = _lookVector.x * rotationSpeed;
+
+        accumulatedRotation += rotationAmount;
+
+        transform.rotation = quaternion.Euler(0,accumulatedRotation, 0);
+    }
+
+    [ServerRpc]
+    private void MoveServerRpc(Vector3 _moveVector)
+    {
+        Move(_moveVector);
+    }
+
+    [ServerRpc]
+    private void LookAroundServerRpc(Vector2 _lookVector)
+    {
+        LookAround(_lookVector);
     }
 }
