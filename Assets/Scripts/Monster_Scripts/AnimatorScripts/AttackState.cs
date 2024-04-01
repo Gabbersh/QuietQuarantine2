b416b3a4d58e14e5087bc1b436bf78cb524bfb09
frajfraj <natvigv@gmail.com>
2024-04-01 11:21:42 +0200

    updatedScripts

--- Assets/Scripts/Monster_Scripts/AnimatorScripts/AttackState.cs
+++ Assets/Scripts/Monster_Scripts/AnimatorScripts/AttackState.cs
@@ -8,7 +8,6 @@ public class AttackState : StateMachineBehaviour
     Transform player;
     NavMeshAgent agent;
 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -20,7 +19,6 @@ public class AttackState : StateMachineBehaviour
         player.transform.LookAt(agent.transform.position);
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.transform.LookAt(player);
@@ -33,21 +31,8 @@ public class AttackState : StateMachineBehaviour
         }
     }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         agent.isStopped = false;
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
